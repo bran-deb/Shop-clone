30420db01ade9420553f56c38612749d77c0cca7
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-04-21 19:33:58 -0400

    feat(Root): add 404 Page not found

---
+++ pages/404.tsx
@@ -0,0 +1,27 @@
+import { NextPage } from "next";
+import Image from "next/image";
+
+import { Box, Typography } from "@mui/material";
+
+import { ShopLayout } from "../components/layouts";
+import image404 from '../public/img/404.jpg'
+
+const Custom404: NextPage = () => {
+    return (
+        <ShopLayout title="Page not found" pageDescription="No hay contenido aqui">
+            <Box
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
+                height='calc(100vh - 200px)'
+                sx={{ flexDirection: { xs: 'column', sm: 'row' } }}//responsive
+            >
+                <Image height={300} width={300} src={image404} alt="pagina no encontrada" />
+                {/* <Typography variant="h1" component='h1' fontSize={60} fontWeight={100}>404 | </Typography> */}
+                <Typography ml={2}>| No se encontro la pagina</Typography>
+            </Box>
+        </ShopLayout>
+    )
+};
+
+export default Custom404;

bebc587d3b7533930927ad82ec164713702119e4
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-04-20 21:00:38 -0400

    feat(Root): add shopLayout & metatags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
+import { ShopLayout } from '../components/layouts'
+import Typography from '@mui/material/Typography'
 
 
 const Home: NextPage = () => {
   return (
-    <h1>Teslo Shop</h1>
+    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos'}>
+      <Typography variant="h1" component='h1'>Tienda</Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
 

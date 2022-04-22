b1da6452ee94318b79996ad23a53e583f64a500c
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-04-21 23:59:29 -0400

    feat(Root): add SideMenu open

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactChild, ReactNode } from "react";
 import Head from "next/head";
 
-import { Navbar } from "../ui";
+import { Navbar, SideMenu } from "../ui";
 
 
 interface Props {
@@ -35,7 +35,7 @@ export const ShopLayout: FC<Props> = ({ title, pageDescription, imageFullUrl, ch
                 <Navbar />
             </nav>
 
-            {/* TODO: sidebar */}
+            <SideMenu />
 
             <main style={{
                 margin: '80px auto',

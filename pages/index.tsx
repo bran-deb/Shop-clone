4a4279cd2f669b03abc25c4aa2edc4ef5fb6cffd
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-04-21 20:28:42 -0400

    feat(Root): add data in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,37 @@
 import type { NextPage } from 'next'
-import { ShopLayout } from '../components/layouts'
+
 import Typography from '@mui/material/Typography'
+import { Card, CardActionArea, CardMedia, Grid } from '@mui/material'
+
+import { initialData } from '../database/products'
+import { ShopLayout } from '../components/layouts'
+
+
 
 
 const Home: NextPage = () => {
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos'}>
       <Typography variant="h1" component='h1'>Tienda</Typography>
-      <Typography variant="h2" sx={{ mb: 1 }}>Todos los productos</Typography>
+      <Typography variant="h2" component='h1' sx={{ mb: 1 }}>Todos los productos</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia          //carga bajo demanda
+                    component={'img'}
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
     </ShopLayout>
   )
 }

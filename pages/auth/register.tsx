bb09ded8b1ec3be41102e263d5bce99b4014e98e
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-04-30 19:52:45 -0400

    feat(Root): add register Page

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,43 +1,60 @@
+import { NextPage } from 'next';
 import NextLink from 'next/link';
+
 import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
-import { AuthLayout } from '../../components/layouts'
 
-const RegisterPage = () => {
-  return (
-    <AuthLayout title={'Ingresar'}>
-        <Box sx={{ width: 350, padding:'10px 20px' }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Typography variant='h1' component="h1">Crear cuenta</Typography>
-                </Grid>
+import { AuthLayout } from '../../components/layouts';
 
-                <Grid item xs={12}>
-                    <TextField label="Nombre completo" variant="filled" fullWidth />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField label="Correo" variant="filled" fullWidth />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField label="Contraseña" type='password' variant="filled" fullWidth />
-                </Grid>
+const RegisterPage: NextPage = () => {
+    return (
+        <AuthLayout title={'Registro'}>
+            <Box sx={{ width: 350, padding: '10px 20px' }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Typography variant='h1' component='h1'>Crear cuenta</Typography>
+                    </Grid>
 
-                <Grid item xs={12}>
-                    <Button color="secondary" className='circular-btn' size='large' fullWidth>
-                        Ingresar
-                    </Button>
-                </Grid>
+                    <Grid item xs={12}>
+                        <TextField label='nombre' variant='filled' fullWidth />
+                    </Grid>
 
-                <Grid item xs={12} display='flex' justifyContent='end'>
-                    <NextLink href="/auth/login" passHref>
-                        <Link underline='always'>
-                            ¿Ya tienes cuenta?
-                        </Link>
-                    </NextLink>
+                    <Grid item xs={12}>
+                        <TextField label='apellido' variant='filled' fullWidth />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <TextField label='correo' type='email' variant='filled' fullWidth />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <TextField label='contraseña' type='password' variant='filled' fullWidth />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <TextField label='confirmar contraseña' type='password' variant='filled' fullWidth />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <NextLink href='/auth/login' passHref>
+                            <Link>
+                                <Button color='secondary' className='circular-btn' fullWidth>
+                                    Crear
+                                </Button>
+                            </Link>
+                        </NextLink>
+                    </Grid>
+
+                    <Grid item xs={12} display='flex' justifyContent='end'>
+                        <NextLink href='/auth/login' passHref>
+                            <Link underline='always'>
+                                Ya tienes una cuenta?
+                            </Link>
+                        </NextLink>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Box>
-    </AuthLayout>
-  )
+            </Box>
+        </AuthLayout>
+    )
 }
 
-export default RegisterPage
+export default RegisterPage;

770284618d2918762e952890ba2c9c461a6ef135
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-05-05 11:11:54 -0400

    feat(Root): add REST getAll products

---
+++ pages/api/products/index.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { db } from '../../../database'
+import { IProduct } from '../../../interfaces'
+import { Product } from '../../../models'
+
+type Data =
+    | { message: string }
+    | IProduct[]
+// | { products: IProduct[] }
+
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+
+    switch (req.method) {
+        case 'GET':
+            return getProducts(req, res)
+        default:
+            return res.status(400).json({
+                message: 'Bad request'
+            })
+    }
+}
+
+const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    await db.connect()
+    /* Selecting the fields that we want to return to the client. */
+    const products = await Product
+        .find()
+        .select('title images price inStock slug -_id')
+        .lean()
+    await db.disconnect()
+
+    return res.status(200).json(
+        products
+    )
+
+}

2ef0cdc486a0fe29bbdb42b447299ed85c34ffdf
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-05-08 23:44:11 -0400

    feat(Root): config global de SWR
    - add provider de SWR
    - add useProducts que obtiene los datos con useSWR
    - obtiene isloading & products de useProducts() con la url del API

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,24 @@ import { Typography } from '@mui/material'
 // import { initialData } from '../database/products'
 import { ShopLayout } from '../components/layouts'
 import { ProductList } from '../components/products'
-
-import useSWR from 'swr'
-const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
+import { useProducts } from '../hooks'
 
 
 const Home: NextPage = () => {
 
-  const { data, error } = useSWR('/api/products', fetcher)
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
-  console.log(data);
+  const { products, isLoading } = useProducts('/products')
 
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos'}>
       <Typography variant="h1" component='h1'>Tienda</Typography>
       <Typography variant="h2" component='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
-      <ProductList products={data} />
+      {
+        isLoading
+          ? <h1>Cargando...</h1>
+          : <ProductList products={products} />
+      }
+
       {/* <ProductList products={initialData.products as any} /> */}
     </ShopLayout>
   )

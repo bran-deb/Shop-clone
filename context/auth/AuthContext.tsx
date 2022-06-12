2cb30def8d6a27b5f57ad484983822764450bfa8
Brand-deb <jairo.padilla.deb19@gmail.com>
2022-06-12 19:47:43 -0400

    feat(Root): add logout method on authContext

--- context/auth/AuthContext.tsx
+++ context/auth/AuthContext.tsx
@@ -8,6 +8,7 @@ interface ContextProps {
     // Methods
     loginUser: (email: string, password: string) => Promise<boolean>;
     registerUser: (name: string, email: string, password: string) => Promise<{ hasError: boolean; message?: string; }>;
+    logoutUser: () => void;
 }
 
 

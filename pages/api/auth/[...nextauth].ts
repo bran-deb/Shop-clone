9a8b993e2c6ab954060acd0227611f66bcc81252
Brand-deb <jairo.padilla.deb19@gmail.com>
2022-06-18 00:12:47 -0400

    feat(Root): add provedor de credenciales nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,45 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import Credentials from 'next-auth/providers/credentials'
 
 
 
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
+
+        Credentials({
+            name: 'Custom Login',
+            credentials: {
+                email: {
+                    label: 'Correo:',
+                    type: 'email',
+                    placeholder: 'correo@google.com'
+                },
+                password: {
+                    label: 'Contraseña:',
+                    type: 'password',
+                    placeholder: 'Contraseña'
+                }
+            },
+            async authorize(credentials) {
+                console.log({ credentials });
+                //TODO: validar contra base de datos
+
+                return { name: 'Juan', correo: 'juan@google.com', role: 'admin' }
+            }
+        }),
+
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
         // ...add more providers here
     ],
+
+    //callbacks
+    callbacks: {
+
+    }
+
 })

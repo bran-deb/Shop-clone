b0562c692798e32d28683ee9a9c358d7190afbf0
Brand-deb <jairo.padilla.deb19@gmail.com>
2022-06-21 18:46:02 -0400

    feat(Root): next auth callbacks

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -40,6 +40,33 @@ export default NextAuth({
     //callbacks
     callbacks: {
 
+        async jwt({ token, account, user }) {
+
+            if (account) {
+                token.accessToken = account.access_token
+
+                switch (account.type) {
+
+                    case "oauth":
+                        //TODO: crear usuario o verificar si existe en mi db
+                        break;
+
+                    case "credentials":
+                        token.user = user
+                        break;
+                }
+            }
+            return token
+        },
+
+
+        async session({ session, token, user }) {
+
+            session.accessToken = token.accessToken
+            session.user = token.user as any
+
+            return session
+        }
     }
 
 })

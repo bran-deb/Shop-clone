faefecd267d5794b089ba307d96be84eb0408975
Brand-db <jairo.padilla.deb19@gmail.com>
2022-08-03 00:00:02 -0400

    fix(Root): fixed getOrderById

--- database/dbOrders.ts
+++ database/dbOrders.ts
@@ -6,7 +6,7 @@ import { db } from '.';
 
 export const getOrderById = async (id: string): Promise<IOrder | null> => {
 
-    if (isValidObjectId(id)) return null
+    if (!isValidObjectId(id)) return null
 
     await db.connect()
     const order = await Order.findById(id).lean()

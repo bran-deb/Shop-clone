be26e9b8e0cfbcff906b5fd5f8ab555409f9b1b3
Brand-db <jairo.padilla.deb19@gmail.com>
2022-08-08 11:35:17 -0400

    feat(Root): add buttons paypal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { SessionProvider } from "next-auth/react"
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 import { UIProvider, CartProvider, AuthProvider } from '@/context';
 import { lightTheme } from '@/themes';
@@ -11,24 +12,26 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <SessionProvider>
-      <SWRConfig
-        // NOTE: provider de SWR
-        value={{
-          fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-          // refreshInterval: 3000,
-        }}
-      >
-        <AuthProvider>
-          <CartProvider>
-            <UIProvider>
-              <ThemeProvider theme={lightTheme}>
-                <CssBaseline />
-                <Component {...pageProps} />
-              </ThemeProvider>
-            </UIProvider>
-          </CartProvider>
-        </AuthProvider>
-      </SWRConfig>
+      <PayPalScriptProvider options={{ 'client-id': process.env.NEXT_PUBLIC_PAYPAL_CLIENT || '' }}>
+        <SWRConfig
+          // NOTE: provider de SWR
+          value={{
+            fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+            // refreshInterval: 3000,
+          }}
+        >
+          <AuthProvider>
+            <CartProvider>
+              <UIProvider>
+                <ThemeProvider theme={lightTheme}>
+                  <CssBaseline />
+                  <Component {...pageProps} />
+                </ThemeProvider>
+              </UIProvider>
+            </CartProvider>
+          </AuthProvider>
+        </SWRConfig>
+      </PayPalScriptProvider>
     </SessionProvider>
   )
 }

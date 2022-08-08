60353a08914ccb96d5e063bc9933f302cce7f4d8
Brand-db <jairo.padilla.deb19@gmail.com>
2022-08-08 17:31:02 -0400

    fix(Root): update nextAuth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,85 +1,92 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import Credentials from 'next-auth/providers/credentials'
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import Credentials from 'next-auth/providers/credentials';
 
-import { dbUsers } from "@/database";
-
-
+import { dbUsers } from '../../../database';
 
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
 
+        // ...add more providers here
+
         Credentials({
             name: 'Custom Login',
             credentials: {
-                email: {
-                    label: 'Correo:',
-                    type: 'email',
-                    placeholder: 'correo@google.com'
-                },
-                password: {
-                    label: 'Contraseña:',
-                    type: 'password',
-                    placeholder: 'Contraseña'
-                }
+                email: { label: 'Correo:', type: 'email', placeholder: 'correo@google.com' },
+                password: { label: 'Contraseña:', type: 'password', placeholder: 'Contraseña' },
             },
             async authorize(credentials) {
-                // return { name: 'Juan', correo: 'juan@google.com', role: 'admin' }
-                //NOTE: validar contra base de datos
-                return await dbUsers.checkUserEmailPassword(credentials!.email, credentials!.password)
+                console.log({ credentials })
+                // return { name: 'Juan', correo: 'juan@google.com', role: 'admin' };
+
+                return await dbUsers.checkUserEmailPassword(credentials!.email, credentials!.password);
+
             }
         }),
 
+
         GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+            clientId: process.env.GITHUB_ID || '',
+            clientSecret: process.env.GITHUB_SECRET || '',
         }),
-        // ...add more providers here
+
+
     ],
-    //custom pages
+
+    // Custom Pages
     pages: {
         signIn: '/auth/login',
-        newUser: 'auth/register'
+        newUser: '/auth/register'
     },
+
+    // Callbacks
     jwt: {
-
+        // secret: process.env.JWT_SECRET_SEED, // deprecated
     },
+
     session: {
-        maxAge: 2592000,     //30d
+        maxAge: 2592000, /// 30d
         strategy: 'jwt',
-        updateAge: 86400,   //cada dia
+        updateAge: 86400, // cada día
     },
 
-    //callbacks
+
     callbacks: {
 
         async jwt({ token, account, user }) {
+            // console.log({ token, account, user });
 
             if (account) {
-                token.accessToken = account.access_token
+                token.accessToken = account.access_token;
 
                 switch (account.type) {
-                    case "oauth":
-                        // crear usuario o verificar si existe en mi db
-                        token.user = await dbUsers.oAuthToDBUser(user?.email || '', user?.name || '')
+
+                    case 'oauth':
+                        token.user = await dbUsers.oAuthToDBUser(user?.email || '', user?.name || '');
                         break;
-                    case "credentials":
-                        token.user = user
+
+                    case 'credentials':
+                        token.user = user;
                         break;
                 }
+
             }
-            return token
+
+            return token;
         },
 
 
         async session({ session, token, user }) {
+            // console.log({ session, token, user });
 
-            session.accessToken = token.accessToken
-            session.user = token.user as any
+            session.accessToken = token.accessToken;
+            session.user = token.user as any;
 
-            return session
+            return session;
         }
+
+
     }
 
-})
+});

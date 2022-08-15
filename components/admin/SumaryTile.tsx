767e93c4acf780e17ebd57a1aa343031a5e3411b
Brand-db <jairo.padilla.deb19@gmail.com>
2022-08-14 23:57:18 -0400

    feat(Root): add pagina principal admin

---
+++ components/admin/SumaryTile.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react"
+import { Grid, Card, CardContent, Typography } from "@mui/material"
+
+
+interface Props {
+    title: string | number
+    subtitle: string
+    icon: JSX.Element
+}
+
+export const SumaryTile: FC<Props> = ({ title, subtitle, icon }) => {
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12} sm={4} md={3}>
+                <Card sx={{ display: 'flex' }}>
+                    <CardContent sx={{ width: 50, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        {icon}
+                    </CardContent>
+
+                    <CardContent sx={{ flex: '1 0 auto', display: 'flex', flexDirection: 'column' }}>
+                        <Typography variant='h3'>{title}</Typography>
+                        <Typography variant='caption'>{subtitle}</Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
+    )
+}
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,16 +1,60 @@
-import React from "react";
-import { DashboardOutlined } from "@mui/icons-material";
+import { FC } from "react";
+import { Grid } from "@mui/material";
+import { DashboardOutlined, CreditCardOffOutlined, AttachMoneyOutlined, GroupAddOutlined, CancelPresentationOutlined, ProductionQuantityLimitsOutlined, CategoryOutlined, AccessTimeOutlined } from '@mui/icons-material';
+
 import { AdminLayout } from "@/components/layouts";
+import { SumaryTile } from "@/components/admin";
 
 
-const DashboardPage = () => {
+const DashboardPage: FC = () => {
     return (
         <AdminLayout
             title='DashBoard'
             subTitle='Estadisticas generales'
             icon={<DashboardOutlined />}
         >
-            <h3>Hola desde DashBoard</h3>
+            <Grid container spacing={2}>
+                <SumaryTile
+                    title={1}
+                    subtitle="Ordenes totales"
+                    icon={<CreditCardOffOutlined color='secondary' sx={{ fontSize: 40 }} />}
+                />
+                <SumaryTile
+                    title={2}
+                    subtitle="Ordenes pagadas"
+                    icon={<AttachMoneyOutlined color='success' sx={{ fontSize: 40 }} />}
+                />
+                <SumaryTile
+                    title={3}
+                    subtitle="Ordenes pendientes"
+                    icon={<CreditCardOffOutlined color='error' sx={{ fontSize: 40 }} />}
+                />
+                <SumaryTile
+                    title={4}
+                    subtitle="Clientes"
+                    icon={<GroupAddOutlined color='primary' sx={{ fontSize: 40 }} />}
+                />
+                <SumaryTile
+                    title={5}
+                    subtitle="Productos"
+                    icon={<CategoryOutlined color='warning' sx={{ fontSize: 40 }} />}
+                />
+                <SumaryTile
+                    title={6}
+                    subtitle="Sin existencias"
+                    icon={<CancelPresentationOutlined color='error' sx={{ fontSize: 40 }} />}
+                />
+                <SumaryTile
+                    title={7}
+                    subtitle="Bajo inventario"
+                    icon={<ProductionQuantityLimitsOutlined color='warning' sx={{ fontSize: 40 }} />}
+                />
+                <SumaryTile
+                    title={8}
+                    subtitle="Actualizacion en:"
+                    icon={<AccessTimeOutlined color='secondary' sx={{ fontSize: 40 }} />}
+                />
+            </Grid>
         </AdminLayout>
     )
 };

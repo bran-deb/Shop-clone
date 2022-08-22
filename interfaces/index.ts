9c78776b256a405a53496671ae984926e57f877e
Brand-db <jairo.padilla.deb19@gmail.com>
2022-08-21 23:59:48 -0400

    feat(Root): mostrar estadisticas en dashboard

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -2,4 +2,5 @@ export * from './cart';
 export * from './products';
 export * from './user';
 export * from './order';
+export * from './dashboard';
 export * as IPaypal from './paypal';
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,12 +1,36 @@
-import { FC } from "react";
-import { Grid } from "@mui/material";
+import { FC, useState } from 'react';
+import { Grid, Typography } from "@mui/material";
 import { DashboardOutlined, CreditCardOffOutlined, AttachMoneyOutlined, GroupAddOutlined, CancelPresentationOutlined, ProductionQuantityLimitsOutlined, CategoryOutlined, AccessTimeOutlined } from '@mui/icons-material';
 
+import { DashboardSummaryResponse } from "@/interfaces";
 import { AdminLayout } from "@/components/layouts";
 import { SumaryTile } from "@/components/admin";
+import useSWR from 'swr';
 
 
 const DashboardPage: FC = () => {
+
+    const { data, error } = useSWR<DashboardSummaryResponse>('/api/admin/dashboard', {
+        refreshInterval: 30 * 1000 // 30 seg
+    })
+    const [refresh, setRefresh] = useState(30);
+
+    if (!data && !error) return <></>
+
+    if (error) {
+        console.log(error)
+        return <Typography>Error al cargar la informacion</Typography>
+    }
+    const {
+        numberOfOrders,
+        paidOrders,
+        numberOfClients,
+        numberOfProducts,
+        productsWithNoInventory,
+        lowInventory,
+        notPaidOrders
+    } = data!
+
     return (
         <AdminLayout
             title='DashBoard'
@@ -15,37 +39,37 @@ const DashboardPage: FC = () => {
         >
             <Grid container spacing={2}>
                 <SumaryTile
-                    title={1}
+                    title={numberOfOrders}
                     subtitle="Ordenes totales"
                     icon={<CreditCardOffOutlined color='secondary' sx={{ fontSize: 40 }} />}
                 />
                 <SumaryTile
-                    title={2}
+                    title={paidOrders}
                     subtitle="Ordenes pagadas"
                     icon={<AttachMoneyOutlined color='success' sx={{ fontSize: 40 }} />}
                 />
                 <SumaryTile
-                    title={3}
+                    title={notPaidOrders}
                     subtitle="Ordenes pendientes"
                     icon={<CreditCardOffOutlined color='error' sx={{ fontSize: 40 }} />}
                 />
                 <SumaryTile
-                    title={4}
+                    title={numberOfClients}
                     subtitle="Clientes"
                     icon={<GroupAddOutlined color='primary' sx={{ fontSize: 40 }} />}
                 />
                 <SumaryTile
-                    title={5}
+                    title={numberOfProducts}
                     subtitle="Productos"
                     icon={<CategoryOutlined color='warning' sx={{ fontSize: 40 }} />}
                 />
                 <SumaryTile
-                    title={6}
+                    title={productsWithNoInventory}
                     subtitle="Sin existencias"
                     icon={<CancelPresentationOutlined color='error' sx={{ fontSize: 40 }} />}
                 />
                 <SumaryTile
-                    title={7}
+                    title={lowInventory}
                     subtitle="Bajo inventario"
                     icon={<ProductionQuantityLimitsOutlined color='warning' sx={{ fontSize: 40 }} />}
                 />
